842bc4bfb0907ee95159dd7c8c62a34f8cfe1b5b
rushi-smb <rushi@smbios.co.in>
2023-02-23 19:36:19 +0530

    Changing the Background color using slider

--- ColorMaker/MainPage.xaml.cs
+++ ColorMaker/MainPage.xaml.cs
@@ -2,10 +2,25 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        var red = sldRed.Value;
+        var green = sldGreen.Value;
+        var blue = sldBlue.Value;
+
+        Color color = Color.FromRgb(red, green, blue);
+        SetColor(color);
+    }
+
+    private void SetColor(Color color)
+    {
+        btnRandom.BackgroundColor = color;
+        Container.Background = color;
+        lblHex.Text = color.ToHex();
+    }
 }
