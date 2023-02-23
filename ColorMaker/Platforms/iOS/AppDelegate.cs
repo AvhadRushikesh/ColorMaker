66b7387b7d75083c27434918f1f2812bddcffebe
rushi-smb <rushi@smbios.co.in>
2023-02-23 18:45:02 +0530

    Specifying colors for use in a Content Page

--- ColorMaker/Platforms/iOS/AppDelegate.cs
+++ ColorMaker/Platforms/iOS/AppDelegate.cs
@@ -5,5 +5,5 @@ namespace ColorMaker;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
