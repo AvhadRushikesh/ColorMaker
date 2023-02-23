7667d8db802b150d92adc0996c0af93a64aad25c
rushi-smb <rushi@smbios.co.in>
2023-02-23 20:27:54 +0530

    Copying the Image Button Color to Clipboard

--- ColorMaker/MainPage.xaml.cs
+++ ColorMaker/MainPage.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System.Diagnostics;
+﻿using CommunityToolkit.Maui.Alerts;
+using Microsoft.Maui.ApplicationModel.DataTransfer;
+using System.Diagnostics;
 
 namespace ColorMaker;
 
 public partial class MainPage : ContentPage
 {
     bool isRandom;
+    string hexValue;
     public MainPage()
     {
         InitializeComponent();
@@ -28,7 +31,8 @@ public partial class MainPage : ContentPage
         Debug.WriteLine(color.ToString());
         btnRandom.BackgroundColor = color;
         Container.Background = color;
-        lblHex.Text = color.ToHex();
+        hexValue= color.ToHex();
+        lblHex.Text = hexValue;
     }
 
     private void btnRandom_Clicked(object sender, EventArgs e)
@@ -45,5 +49,14 @@ public partial class MainPage : ContentPage
         sldGreen.Value= color.Green;
         sldBlue.Value= color.Blue;
         isRandom= false;
+    }
+
+    private async void ImageButton_Clicked(object sender, EventArgs e)
+    {
+        await Clipboard.SetTextAsync(hexValue);
+        var toast = Toast.Make("Color Copied",
+            CommunityToolkit.Maui.Core.ToastDuration.Short,
+            12);
+        await toast.Show();
     }
 }
